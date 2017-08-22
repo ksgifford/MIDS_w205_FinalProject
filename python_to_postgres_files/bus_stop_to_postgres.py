ceb95bfebfe8ee1b9d72534935c52c3d4ed43ea7
mikegruzynski <mike.gruzynski@berkeley.edu>
2017-08-22 02:44:09 -0700

    Update bus_stop_to_postgres.py

--- python_to_postgres_files/bus_stop_to_postgres.py
+++ python_to_postgres_files/bus_stop_to_postgres.py
@@ -1,31 +1,19 @@
 import psycopg2
-
+# connect to postgres database that was previously made (step 6 in readme.md)
 conn = psycopg2.connect(database="final_project", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
-
-
-
+# this is raw data on bus stops with no header file (cleaned in bash.sh file)
 file = open('/data/MIDS_w205_FinalProject/clean_data/bus/stops_noH.txt', 'r')
 lines = file.readlines()
-
+# loop through the files and pick out only information needed for our application (name of variables are relevant)
 for line in lines:
     split_line = line.split(",")
-
     stop_id_py = int(split_line[0])
-    # stop_code_py = split_line[1]
     stop_name_py = split_line[2]
-    # stop_desc_py = split_line[3]
     stop_lat_py = float(split_line[4])
     stop_lon_py = float(split_line[5])
-    # zone_id_py = split_line[6]
-    # stop_url_py = split_line[7]
-    # location_type_py = float(split_line[8])
-    # parent_station_py = split_line[9]
-    # stop_timezone_py = split_line[10]
-
     cur.execute("INSERT INTO bus_stop_seattle (stop_id, stop_name, stop_lat, stop_lon) VALUES (%s, %s, %s, %s);", (stop_id_py, stop_name_py, stop_lat_py, stop_lon_py))
     conn.commit()
-
     print(stop_id_py)
 print("----------------------")
 print("SCRIPT DONE")
